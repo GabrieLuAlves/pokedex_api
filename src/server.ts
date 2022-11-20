afcd5a1652cee667963ab5231176548e91dd4a68
Gabriel Luan Alves Valentim <gabrielluan.valentim@gmail.com>
2022-11-20 19:29:39 -0300

    Base code

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (request: Request, response: Response) => {
+  return response.json({
+    message: "Hello world"
+  })
+});
+
+app.listen(3333, "localhost", () => {
+  console.log(`Server listening at: http://localhost:3333`)
+});
