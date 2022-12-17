03cc27ed39cbcebceb0a3aba9f3eb1ecd952bbb3
Grazi Oliveira <devgrazioliveira@gmail.com>
2022-12-17 18:57:13 -0300

    fix: put promise error

--- src/controllers/UpdatePokemonController.ts
+++ src/controllers/UpdatePokemonController.ts
@@ -5,7 +5,6 @@ import { container } from "tsyringe";
 class UpdatePokemonController {
   async handle(request: Request, response: Response) {
     const { id, name, weight, typesIds, eggGroupId } = request.body;
-
     const updatePokemonService = container.resolve(UpdatePokemonService)
 
     return response.json(updatePokemonService.execute({ id, name, weight, typesIds, eggGroupId }));
